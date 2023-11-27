de72cab48a6d9a9f36f0ed627deba7d1274f41c4
lafetz <yafetyohaness@gmail.com>
2023-11-27 15:14:12 +0300

    add scores

--- guess.sh
+++ guess.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 echo wellcome to the guessing game!
+echo enter you name
+read name
 answer=true
 random=$(( ( RANDOM % 10 )  + 1 ))
-while [ $answer ]
-do
+total_tries=0
 echo guess the number from 1 to 10
+while [ $answer ]
 read guess
+do
 if [ $guess -eq $random ];then
     echo correct
+    echo $name  $total_tries >> scores.txt
     break
 else 
    echo incorrect guess again
